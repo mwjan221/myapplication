985cc690a1d2e16ea989f5a8cdb1bfbe1aa5fe18
Mark walsh <Mark@xyz.com>
2022-01-21 14:44:14 +1100

    add web applicaiton code

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace myapplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
